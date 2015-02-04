c1507787a6ce686e408eaa7c0faa27cd3bbf14ec
Sadi Hossain <sadi.f.hos@gmail.com>
2015-02-04 12:53:42 +0200

    Initial push

---
+++ samubot/SamuBot.java
@@ -0,0 +1,56 @@
+package samubot;
+
+import java.util.List;
+import java.util.Random;
+
+import fi.zem.aiarch.game.hierarchy.Engine;
+import fi.zem.aiarch.game.hierarchy.Move;
+import fi.zem.aiarch.game.hierarchy.Player;
+import fi.zem.aiarch.game.hierarchy.Side;
+import fi.zem.aiarch.game.hierarchy.Situation;
+
+public class SamuBot implements Player {
+	public SamuBot(Random rnd) {
+		this.rnd = rnd;
+	}
+	
+	public void start(Engine engine, Side side) {
+	}
+	
+	public Move move(Situation situation, int timeLeft) {
+		List<Move> moves = situation.legal();
+		
+		
+		
+		return moves.get(rnd.nextInt(moves.size()));
+	}
+	
+	private Random rnd;
+
+	private static int maxi( int depth ) {
+		if ( depth == 0 ) return evaluate();
+		int max = -10000;
+		for ( all moves) {
+			score = mini( depth - 1 );
+			if( score > max )
+				max = score;
+		}
+		return max;
+	}
+	
+	private static int mini( int depth ) {
+	    if ( depth == 0 ) return -evaluate();
+	    int min = 10000;
+	    for ( all moves) {
+	        score = maxi( depth - 1 );
+	        if( score < min )
+	            min = score;
+	    }
+	    return min;
+	}
+	
+	private static int elavuate () {
+		return 0;
+	}
+	}
+}
