bb35eecc8ee9e7b0a42eb7df5e78f0cba2654b48
Valyrian <teemu.belt@gmail.com>
2015-04-02 17:55:21 +0300

    Fixed attack score in eval

--- samubot/Evaluator.java
+++ samubot/Evaluator.java
@@ -100,38 +100,10 @@ public class Evaluator {
 		Iterable<Board.Square> pieces = board.pieces(currentSide);
 		for (Board.Square square : pieces){
 			result+=evalPiece(situation, move, currentSide, board, square, lastPiece);
-//			int x = square.getX();
-//			int y = square.getY();
-//			int value = board.get(x, y).getValue();
-//			
-//			//Sum up piece ranks
-//			ranks+=value;
-//			if(!board.owner(x, y).equals(currentSide)){ //not in own starting area
-//				//Pieces capable of attacking
-//				attackers+=1;
-//				if(!situation.isBlocked(x, y)){
-//					//Combined attack distance
-//					temp=board.firepower(currentSide, x, y);
-//					if(temp>0)
-//						firepower+=temp;
-//				}
-//				//If piece is open to attacking side
-//				exposed = exposure(currentSide, board, x, y, value);
-//				
-//					
-//			}
-//			//Distance of the highest ranking piece from own corner
-//			if(value == maxPiece)
-//				kingPosition = distanceFromHome(currentSide, x,y);
-//			
-//			//If only one enemy piece left
-//			if(lastPiece != null)
-//				//Pieces proximity to the enemy's piece
-//				distanceToKing = w-Math.abs(lastPiece.getX()-x)+h-Math.abs(lastPiece.getY()-y);
 		}
 		
 		//Value of a piece under attack
-		if(move.getType() == MoveType.ATTACK){
+		if(move.getType() == MoveType.ATTACK && move.getPlayer().equals(currentSide)){
 			double value = move.getTarget().getValue();
 			if(value==maxPiece)
 				value = kingValue;
@@ -141,17 +113,11 @@ public class Evaluator {
 		
 		result+= attack*attackX;
 		
-//		result+=ranks*ranksX;
-//		result+=attackers*attackersX;
-//		result+=firepower*firepowerX;
-//		result+=exposed*exposedX;
-//		result+=kingPosition*kingPositionX;
-//		result+=distanceToKing*distanceToKingX;
-		
 		return(result);
 	}
 	
 	private double evalPiece(Situation situation, Move move, Side currentSide, Board board, Board.Square square, Board.Square lastPiece){
+		// Evaluation values
 		double ranks = 0;
 		double attackers = 0;
 		double firepower = 0;
